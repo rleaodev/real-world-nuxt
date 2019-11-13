c4defa8a3c78f8f8118f0cfaf26e2c8648a093b7
Rafael Nascimento de Souza Leão <rleaodev@gmail.com>
2019-11-12 23:34:04 -0300

    Using axios to fetch events

---
+++ pages/event/create.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Create</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Create an awesome event'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
+  components: {
+    EventCard
+  },
   head() {
     return {
       title: 'Event Listing'
     }
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:8000/events')
+      .then((response) => {
+        return {
+          events: response.data
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again'
+        })
+      })
   }
 }
 </script>
